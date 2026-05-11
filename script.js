abfaefca5a691dfff165338fb56500187628d46e
marcos ronaldinho <zodkdos1@gmail.com>
2026-05-11 11:34:48 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 const botoes = document.querySelectorAll("botao");
 for (let 1=0); 1 <botoes.lenght; i++){
-
+  botoes [i].onclick = function (){
+    botoes [i].classList(" ativo")
+  }
 console.log("botoes");
 }
