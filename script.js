47a0ca2e72a3ad52cb1ec7cb3da1b09b11e018b1
marcos ronaldinho <zodkdos1@gmail.com>
2026-05-25 11:10:38 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,13 +1,46 @@
-const = Document.querySelectorAll (".botoa");
-const = documento.querySelectorAll ("aba-conteudo");
-for (let i=0; i <botoes.lenght; i++ ){
-    botoes [i].onclick = function (){
-        for(let j=0; j<botoes.lenght; j++){
+const botoes = document.querySelectorAll(".botao");
+const textos = document.querySelectorAll(".aba-conteudo");
+
+for (let i = 0; i < botoes.length; i++) {
+    botoes[i].onclick = function() {
+        for (let j = 0; j < botoes.length; j++) {
             botoes[j].classList.remove("ativo");
             textos[j].classList.remove("ativo");
         }
-        botoes[i].classlist.add("ativo")
-        textos[i].classlist.add("ativo")
+        botoes[i].classList.add("ativo");
+        textos[i].classList.add("ativo");
     }
-    console.log(botoes);
+}
+
+const contadores = document.querySelectorAll(".contador");
+const tempoObjetivo1 = new Date("2026-12-28T23:59:59");
+const tempoObjetivo2 = new Date("2026-12-28T23:59:59");
+const tempoObjetivo3 = new Date("2026-12-28T23:59:59");
+const tempoObjetivo4 = new Date("2026-12-28T23:59:59");
+
+const tempos = [
+    tempoObjetivo1, tempoObjetivo2, tempoObjetivo3, tempoObjetivo4
+];
+
+for (let i = 0; i < contadores.length; i++) {
+    contadores[i].textContent = calculaTempo(tempos[i]);
+}
+contadores[0].textContent = calculaTempo(tempoObjetivo1);
+
+function calculaTempo(tempoObjetivo1) {
+    let tempoAtual = new Date();
+    let tempoFinal = tempoObjetivo1 - tempoAtual;
+    let segundos = Math.floor(tempoFinal / 1000);
+    let minutos = Math.floor(segundos / 60);
+    let horas = Math.floor(minutos / 60);
+    let dias = Math.floor(horas / 24);
+
+    segundos %= 60;
+    minutos %= 60;
+    horas %= 24;
+
+    return dias + " dias "
+        + horas + " horas "
+        + minutos + " minutos "
+        + segundos + " segundos ";
 }
